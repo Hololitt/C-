92cccb699e8cbf3c168d48d046d912c7b74bb134
Nikon <109473540+Hololitt@users.noreply.github.com>
2023-01-03 23:17:15 +0100

    Add files via upload

---
+++ Lesson Enum 13.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+enum list{firstNumber = 89, secondNumber = 12, thirdNumber = 4};
+int main(int argc, const char * argv[]){
+cout<<firstNumber << " " << secondNumber << " " << thirdNumber;
+
+
+    cin.get();
+    return 0;
+}
